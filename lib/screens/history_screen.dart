873443b5a7f12dde0959f84c5d1a192486c73594
Nischal <nischal.sha17@gmail.com>
2023-02-15 17:20:43 +0545

    history screen created

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,14 +1,55 @@
-import 'package:flutter/material.dart';
-// import 'dart:html';
+import 'dart:convert';
+import 'dart:developer';
 
-class HistoryScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../url.dart';
+import 'package:http/http.dart' as http;
+import '../providers/all_data.dart';
+import 'package:provider/provider.dart';
+
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
 
   @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  late List data;
+  bool isLoading = true;
+  @override
+  void didChangeDependencies() async {
+    String token = Provider.of<AllData>(context).token;
+
+    var response = await http.get(Uri.parse(getHistoryUrl),
+        headers: {"Authorization": "Token $token"});
+    data = json.decode(response.body);
+    setState(() {
+      isLoading = false;
+    });
+
+    log(response.body.toString());
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("History"),
-    ));
+    return Scaffold(
+        body: isLoading
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Column(
+                      children: [
+                        Text(data[index]["description"]),
+                        Text(data[index]["created_at"]),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              ));
   }
 }
