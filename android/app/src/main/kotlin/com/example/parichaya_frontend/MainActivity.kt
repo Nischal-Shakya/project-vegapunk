6544933f2274137c112724559866ac74cccaa5ae
Nischal <nischal.sha17@gmail.com>
2023-03-18 13:48:38 +0545

    No screenshots allowed

--- android/app/src/main/kotlin/com/example/parichaya_frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/parichaya_frontend/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.parichaya_frontend
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.engine.FlutterEngine
+import android.view.WindowManager.LayoutParams
 import androidx.annotation.NonNull;
 
 class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
